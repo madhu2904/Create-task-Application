6000e0597a9b79049d4ae31a6c3415e38f361686
Ranjith <ranjith3arumugam@gmail.com>
2026-04-14 23:28:18 +0530

    Project entity class created

---
+++ Create-Task-Application/src/main/java/com/batch5/Create_Task_Application/ProjectModule/entity/Project.java
@@ -0,0 +1,41 @@
+package com.batch5.Create_Task_Application.ProjectModule.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "project")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
+    private Integer projectId;
+
+    @Column(name = "project_name", nullable = false)
+    private String projectName;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private List<Task> tasks;
+}
