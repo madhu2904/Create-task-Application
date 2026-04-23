52e481c0270cd1556ff36cb59f6100ed0e16d8f4
sarmisha-29 <sarmishaaids@gmail.com>
2026-04-23 22:27:21 +0530

    Made some in routing

---
+++ Frontend/task-manager-ui/src/app/modules/task/task.routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+
+import { moduleAuthGuard } from '../common/guards/module-auth.guard';
+import { TaskDashboardComponent } from './pages/task-dashboard/task-dashboard.component';
+import { TaskEndpointComponent } from './pages/task-endpoint/task-endpoint.component';
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: TaskDashboardComponent,
+    canActivate: [moduleAuthGuard],
+    data: { moduleKey: 'task' },
+    title: 'Task Dashboard',
+  },
+  {
+    path: ':endpointKey',
+    component: TaskEndpointComponent,
+    canActivate: [moduleAuthGuard],
+    data: { moduleKey: 'task' },
+    title: 'Task Endpoint',
+  },
+];
